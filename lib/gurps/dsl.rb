4b324d169f742ad698b1ddda9654d0814d02f3cb
Alex Safatli <safatli@cs.dal.ca>
2015-09-25 16:17:25 -0300

    Started work on Template DSL

--- lib/gurps/dsl.rb
+++ lib/gurps/dsl.rb
@@ -6,25 +6,32 @@ module GURPS
 
   class CharacterProxy < Character
     
+    # Shorthands
+
     include AttributeShorthands
     include TemplateShorthands
 
   end
 
-  module Characters
+  class TemplateProxy < Template
 
-    # Domain Specific Language for Character Creation
+    # Shorthands
 
-    def self.make name, &block
-      @character = CharacterProxy.new name: name
-      @character.instance_eval(&block)
-      @character
-    end
+    # @todo
 
   end
 
+
   def self.character name, &block
-    Characters.make name, &block
+    @character = CharacterProxy.new name: name
+    @character.instance_eval(&block)
+    @character
+  end
+
+  def self.template name, &block
+    @template = TemplateProxy.new name 
+    @template.instance_eval(&block)
+    @template 
   end
 
 end
