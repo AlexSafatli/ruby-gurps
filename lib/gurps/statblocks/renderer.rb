5ba6dafd15a9d83921a41a21d68e221ca5ca0f2d
Alex Safatli <safatli@cs.dal.ca>
2015-12-23 11:54:40 -0400

    Baka.

--- lib/gurps/statblocks/renderer.rb
+++ lib/gurps/statblocks/renderer.rb
@@ -1,14 +1,20 @@
 require 'erb'
 
 module GURPS
+
   module Statblocks
+
     module Renderer
+
       include ERB::Util
       def render(opts={with: 'data/templates/render.html.erb'})
         template_str = File.read(opts[:with])
         renderer = ERB.new(template_str).result(binding)
         File.open(opts[:file],'w+') { |f| f.write(renderer) }
       end
+
     end
+
   end
+  
 end
